c3dd3cdbe90429b74677ba5c01ed49bc48c9485e
Elise Phan <aliceconnic@gmail.com>
2019-05-08 22:13:20 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
@@ -52,3 +53,72 @@ export class RestaurantInput extends Component {
 
 //connect this component by wrapping RestaurantInput below
 export default connect(null, { addRestaurant })(RestaurantInput);
+=======
+import React, { Component } from 'react';
+import { addRestaurant } from '../actions/restaurants';
+import { connect } from 'react-redux';
+
+export class RestaurantInput extends Component {
+
+  state = {
+    name: '',
+    location: ''
+  }
+
+  handleOnNameChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleOnLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    // add missing code
+  }
+
+  render() {
+    return(
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
+            id="name"
+            placeholder="restaurant name" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
+            id="location"
+            placeholder="location" />
+        </p>
+        <input type="submit" />
+      </form>
+    );
+  }
+};
+
+
+//connect this component by wrapping RestaurantInput below
+
+const mapStateToProps = state => {
+  return { restaurants: state.restaurants }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addRestaurant,
+  }, dispatch);
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+>>>>>>> 20875a1c61716a1b20187c1463b31d1fb2e3a7a5
